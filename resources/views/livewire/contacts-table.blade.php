030a7ee793279818527e02e5b6d6e3f60786946b
Caleb Porzio <calebporzio@gmail.com>
2019-11-19 13:30:14 -0500

    finished

--- resources/views/livewire/contacts-table.blade.php
+++ resources/views/livewire/contacts-table.blade.php
@@ -2,33 +2,45 @@
     <div class="row mb-4">
         <div class="col form-inline">
             Per Page: &nbsp;
-            <select class="form-control" wire:model="perPage">
+            <select wire:model="perPage" class="form-control">
                 <option>10</option>
                 <option>15</option>
                 <option>25</option>
             </select>
         </div>
         <div class="col">
-            <input class="form-control" type="text" placeholder="Search Contacts..." wire:model="query">
+            <input wire:model="query" type="text" class="form-control" placeholder="Search Contacts...">
         </div>
     </div>
 
-    <table class="table is-fullwidth">
+    <table class="table">
         <thead>
             <tr>
-                <th><a href="#" wire:click.prevent="sortBy('name')">Name @if ($sortField === 'name') {!! $sortAsc ? '&#9660;' : '&#9650;' !!} @endif</a></th>
-                <th><a href="#" wire:click.prevent="sortBy('email')">Email @if ($sortField === 'email') {!! $sortAsc ? '&#9660;' : '&#9650;' !!} @endif</a></th>
-                <th><a href="#" wire:click.prevent="sortBy('birthdate')">Birthdate @if ($sortField === 'birthdate') {!! $sortAsc ? '&#9660;' : '&#9650;' !!} @endif</a></th>
-                <th>Actions</th>
+                <th>
+                    <a href="#0" role="button" wire:click="sortBy('name')">
+                        Name @include('livewire._sort-icon', ['field' => 'name'])
+                    </a>
+                </th>
+                <th>
+                    <a href="#0" role="button" wire:click="sortBy('email')">
+                        Email @include('livewire._sort-icon', ['field' => 'email'])
+                    </a>
+                </th>
+                <th>
+                    <a href="#0" role="button" wire:click="sortBy('birthdate')">
+                        Birthdate @include('livewire._sort-icon', ['field' => 'birthdate'])
+                    </a>
+                </th>
+                <th aria-label="Actions"></th>
             </tr>
         </thead>
         <tbody>
             @foreach ($contacts as $contact)
-                <tr wire:key="{{ $contact->id }}">
-                    <td>{{ $contact->name }}</td>
+                <tr>
+                    <th>{{ $contact->name }}</th>
                     <td>{{ $contact->email }}</td>
-                    <td>{{ $contact->birthdate->format('m/d/Y') }}</td>
-                    <td>
+                    <td>{{ $contact->birthdate->format('m/d/y') }}</td>
+                    <td class="text-right">
                         <a href="#">Edit</a>
                         <a href="#">Remove</a>
                     </td>
@@ -41,8 +53,8 @@
         <div class="col">
             {{ $contacts->links() }}
         </div>
-        <div class="col text-right text-secondary">
-            Showing {{ $contacts->count() }} results of {{ $contacts->total() }}
+        <div class="col text-secondary text-right">
+            Showing {{ $contacts->firstItem() }} to {{ $contacts->lastItem() }} of {{ $contacts->total() }} results
         </div>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,12 @@
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+        <script src="https://kit.fontawesome.com/107c56b88c.js" crossorigin="anonymous"></script>
         @livewireAssets
     </head>
     <body>
         <div class="container pt-4">
-            @livewire('contacts-table')
+            @livewire('foo')
         </div>
 
         <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
